d8c418b2c06c70c92a740407b765a81e5eee7032
stalin-21 <stalinguna21@gmail.com>
2025-06-26 15:12:01 +0530

    UC2: Implement addAtHead method to build linked list in reverse order.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-       Node<Integer> node1 = new Node<>(56);
-        Node<Integer> node2 = new Node<>(30);
-        Node<Integer> node3 = new Node<>(70);
+        LinkedList<Integer> list = new LinkedList<>();
 
-        node1.next = node2;
-        node2.next = node3;
+        list.addAtHead(70);
+        list.addAtHead(30);
+        list.addAtHead(56);
 
-       printList(node1);
+        list.printList();
 
-    }
-
-    public static <T> void printList(Node<T> head) {
-        Node<T> current = head;
-
-        while (current != null) {
-            System.out.print(current.data);
-
-            if (current.next != null) {
-                System.out.print(" -> ");
-            }
-            current = current.next;
-        }
-        System.out.println();
     }
 }
