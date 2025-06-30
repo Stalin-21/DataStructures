41cdce2bdd7e805d1167e713831bc1b5473fd07c
stalin-21 <stalinguna21@gmail.com>
2025-06-30 14:22:45 +0530

    UC3 :Implement UC3 queue creation using LinkedList append.

--- src/stackqueue/Queue.java
+++ src/stackqueue/Queue.java
@@ -1,4 +1,19 @@
 package stackqueue;
 
-public class Queue {
+import linkedlist.LinkedList;
+
+public class Queue<T> {
+    private LinkedList<T> linkedList;
+
+    public Queue() {
+        linkedList = new LinkedList<>();
+    }
+
+    public void enqueue(T data) {
+        linkedList.append(data);
+    }
+
+    public void printQueue() {
+        linkedList.printList();
+    }
 }
