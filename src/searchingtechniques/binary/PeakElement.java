c13e57a3406e0d7466340542fc81bdfcbec0830d
stalin-21 <stalinguna21@gmail.com>
2025-07-11 15:32:32 +0530

    Added a program for sing peak element using binary search.

--- src/searchingtechniques/binary/PeakElement.java
+++ src/searchingtechniques/binary/PeakElement.java
@@ -1,4 +1,29 @@
 package searchingtechniques.binary;
 
 public class PeakElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 20, 4, 1, 0}; // 20 is a peak
+        int peakIndex = findPeakElement(arr);
+        System.out.println("Peak element found at index: " + peakIndex);
+        System.out.println("Peak element is: " + arr[peakIndex]);
+    }
+
+    public static int findPeakElement(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                // Peak is on the right
+                left = mid + 1;
+            } else {
+                // Peak is on the left (or mid)
+                right = mid;
+            }
+        }
+
+        return left; // or right, both are same here
+    }
 }
